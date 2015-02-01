3dd15e3f47d310abc09de01ad7b718fc4d465fce
c-prakash <cprakash@outlook.com>
2015-02-01 22:46:14 +0000

    Added extension method to retrieve ClaimsPrincipal from User class

---
+++ Framework/Sitecore.Extensions/Helpers/UserExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Security.Accounts;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace Framework.Sc.Extensions.Helpers
+{
+    public static class UserExtensions
+    {
+        public static ClaimsPrincipal ToClaimsPrincipal(this User user)
+        {
+            var innerUserField = user.GetType().GetField("_innerUser", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (innerUserField != null)
+                return innerUserField.GetValue(user) as ClaimsPrincipal;
+
+            return null;
+        }
+    }
+}
--- Modules/Common/Controllers/AccountController.cs
+++ Modules/Common/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Common.Models;
+using Framework.Sc.Extensions.Helpers;
 using Framework.Sc.Extensions.Mvc;
 using Framework.Sc.Extensions.Mvc.Filters;
 using Sitecore.Security.Authentication;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace Common.Controllers
@@ -12,18 +14,27 @@ namespace Common.Controllers
         [ImportResult]
         public ActionResult Index()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                var claimsPrincipal = (User as Sitecore.Security.Accounts.User).ToClaimsPrincipal();
+            }
+
             return View(new LoginModel());
         }
 
         [HttpPost]
         [ExportResult]
-        public ActionResult Index(LoginModel login)
+        public  ActionResult Index(LoginModel login)
         {
             if (ModelState.IsValid)
             {
                 if (login.UserName == "test" && login.Password == "test")
                 {
-                    AuthenticationManager.Login("test", false);
+                    Task<string> authTask = new SimpleAuthenticationService().AuthenticateUserAsync(login.UserName, login.Password); 
+                    System.Diagnostics.Debug.WriteLine("Authentication started"); 
+                    string userId = authTask.Result;
+
+                    AuthenticationManager.Login(userId, false);
                     return Redirect(ControllerContext.HttpContext.Request.RawUrl);
                 }
             }
@@ -31,4 +42,16 @@ namespace Common.Controllers
             return View(login);
         }
     }
+
+
+    public sealed class SimpleAuthenticationService
+    {
+        public async Task<string> AuthenticateUserAsync(string user, string password)
+        {
+            //await Task.Delay(5000);
+
+            return user;
+        }
+    }
+
 }
